56a76e892fe58df498a1455db2a3002ca48f41cd
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-20 17:24:06 +0530

    Changes

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -4,4 +4,4 @@
 echo "List of Certificates for $Envi that are going to expire is as follows." > confluence
 echo "=================================================================" >> confluence
 echo "">> confluence
-python bit-bucket/pcf_automation/aws-checker/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> confluence
+python git-test/conf/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> confluence
