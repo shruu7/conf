1902bac8c62445882c4de0b9f9d8a98d2384ffef
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-25 20:21:14 +0530

    add

--- aws-cert-output-py-1.sh
+++ aws-cert-output-py-1.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 #!/usr/bin/env python2
-
+mv confluence confluence-final
 cat confluence-final/confluence
 echo "List of Certificates for $Envi that are going to expire is as follows." >> confluence-final/confluence
 echo "=================================================================" >> confluence-final/confluence
