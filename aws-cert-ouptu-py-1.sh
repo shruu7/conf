70dcbbb3764c5ff9e453ed74300ec27d69c24efb
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-27 21:01:12 +0530

    sdkcks

--- aws-cert-ouptu-py-1.sh
+++ aws-cert-ouptu-py-1.sh
@@ -5,6 +5,7 @@ echo "List of Certificates for $Envi that are going to expire is as follows."
 echo "=================================================================" 
 echo "" 
 var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 2>&1)
+echo "second step" >> newfile.txt
 echo $var1 >> newfile.txt
 echo "-------------"
 cat newfile.txt
