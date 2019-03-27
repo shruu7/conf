1332e33e0e5754353d445af35d46e43f1fc50a3e
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-27 18:24:48 +0530

    sdkcks

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 #!/usr/bin/env python2
 
-echo "List of Certificates for $Envi that are going to expire is as follows." >> confluencesandbox/confluencesandbox
-echo "=================================================================" >> confluencesandbox/confluencesandbox
-echo "">> confluencesandbox/confluencesandbox
-python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> confluencesandbox/confluencesandbox
-cat confluencesandbox/confluencesandbox
+ls
+echo "List of Certificates for $Envi that are going to expire is as follows." 
+echo "=================================================================" 
+echo ""
+python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 
