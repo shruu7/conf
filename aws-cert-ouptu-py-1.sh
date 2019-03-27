5b3e63f3a8365aadb70b824d9571e7f325c06182
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-27 16:16:48 +0530

    sdjkfhgf

--- aws-cert-ouptu-py-1.sh
+++ aws-cert-ouptu-py-1.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 #!/usr/bin/env python2
 
-mv confluencesandbox/confluencesandbox
 echo "List of Certificates for $Envi that are going to expire is as follows." >> confluencenonprod/confluencenonprod
 echo "=================================================================" >> confluencenonprod/confluencenonprod
-echo "">> confluencenonprod
+echo "">> confluencenonprod/confluencenonprod
 python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> confluencenonprod/confluencenonprod
 cat confluencenonprod/confluencenonprod
