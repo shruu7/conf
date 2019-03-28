8152174152b09801b61e0412e24b218dc48e5b6d
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-28 09:07:46 +0530

    sdkcks

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -5,14 +5,14 @@ var=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 var2=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 
-if [ $Envi == sandbox]
+if [ $Envi = "sandbox"]
 then 
     echo "List of Certificates for $Envi that are going to expire is as follows." 
     echo "=================================================================" 
     echo ""
     echo "first step" > newfile.txt
     echo $var >> newfile.txt
-elif [ $Envi == nonprod ]
+elif [ $Envi = "nonprod" ]
 then
     echo "----second step-----" >> newfile.txt
     echo $var1 >> newfile.txt
