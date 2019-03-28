7077eece12d26768981fe495a405e2b51376538e
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-28 09:46:59 +0530

    kjf

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 #!/usr/bin/env python2
 
-var=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
-var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
+
+
 var2=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 
-if [[ $Envi == "sandbox" ]];
-then 
-    echo "List of Certificates for $Envi that are going to expire is as follows." 
-    echo "=================================================================" 
-    echo ""
-    echo "first step" > newfile.txt
-    echo $var >> newfile.txt
-elif [[ $Envi == "nonprod" ]];
-then
-    echo "----second step-----" >> newfile1.txt
-    echo $var1 >> newfile1.txt
-fi
-cat newfile.txt
-cat newfile1.txt
+echo "first step" > newfile.txt
+echo "List of Certificates for Sanbox that are going to expire is as follows." >> newfile.txt
+echo "=================================================================" >> newfile.txt
+echo "" >> newfile.txt
+var=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
+echo $var >> newfile.txt
+
+
+echo "Second step" >> newfile.txt
+echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
+echo "=================================================================" >> newfile.txt
+echo "" >> newfile.txt
+var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
+echo $var1 >> newfile.txt
+   
+
