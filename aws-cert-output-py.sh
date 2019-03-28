76da39a7138c1361662648a7a920db811c8c7337
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-28 09:50:32 +0530

    kjf

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
 #!/usr/bin/env python2
 
-
-
-var2=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
-
 echo "first step" > newfile.txt
 echo "List of Certificates for Sanbox that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
 var=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 echo $var >> newfile.txt
-
 
 echo "Second step" >> newfile.txt
 echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
@@ -20,5 +15,12 @@ echo "" >> newfile.txt
 var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
 echo $var1 >> newfile.txt
    
+echo "Third step" >> newfile.txt
+echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
+echo "=================================================================" >> newfile.txt
+echo "" >> newfile.txt
+var2=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
+echo $var2 >> newfile.txt
+
 
 cat newfile.txt
