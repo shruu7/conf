22a611242d218ebdff2f19815074ff4b3ab7ac42
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-29 18:24:24 +0530

    dnjsd

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -9,14 +9,14 @@ echo "=================================================================" >> newf
 echo "" >> newfile.txt
 export AWS_PROFILE=$Envi; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
 
-aws configure set region us-east-1 --profile nonprod
-aws configure set aws_access_key_id $access-key-id-non  --profile nonprod
-aws configure set aws_secret_access_key $secret-access-key-non --profile nonprod
-echo "Second step" >> newfile.txt
-echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
-echo "=================================================================" >> newfile.txt
-echo "" >> newfile.txt
-export AWS_PROFILE=$envi-non; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
+#aws configure set region us-east-1 --profile nonprod
+#aws configure set aws_access_key_id $access-key-id-non  --profile nonprod
+#aws configure set aws_secret_access_key $secret-access-key-non --profile nonprod
+#echo "Second step" >> newfile.txt
+#echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
+#echo "=================================================================" >> newfile.txt
+#echo "" >> newfile.txt
+#export AWS_PROFILE=$envi-non; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
 
 #aws configure set region us-east-1 --profile prod
 #echo "Third step" >> newfile.txt
