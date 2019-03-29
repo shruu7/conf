cd3e60b495d21f34bb8915c5360f57fda4fa7f80
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-03-29 18:14:44 +0530

    dnjsd

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -1,26 +1,29 @@
 #!/bin/sh
 #!/usr/bin/env python2
-
+aws configure set region us-east-1 --profile sandbox
+aws configure set aws_access_key_id $access-key-id  --profile sandbox
+aws configure set aws_secret_access_key $secret-access-key --profile sandbox
 echo "first step" > newfile.txt
 echo "List of Certificates for Sanbox that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
-var=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
-echo $var >> newfile.txt
+export AWS_PROFILE=$Envi; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
 
+aws configure set region us-east-1 --profile nonprod
+aws configure set aws_access_key_id $access-key-id-non  --profile nonprod
+aws configure set aws_secret_access_key $secret-access-key-non --profile nonprod
 echo "Second step" >> newfile.txt
 echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
-var1=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
-echo $var1 >> newfile.txt
-   
-echo "Third step" >> newfile.txt
-echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
-echo "=================================================================" >> newfile.txt
-echo "" >> newfile.txt
-var2=$(python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90)
-echo $var2 >> newfile.txt
+export AWS_PROFILE=$envi-non; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
+
+#aws configure set region us-east-1 --profile prod
+#echo "Third step" >> newfile.txt
+#echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
+#echo "=================================================================" >> newfile.txt
+#echo "" >> newfile.txt
+#python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
 
 
 cat newfile.txt
