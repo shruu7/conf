78969b0ba35c2660e3f9adc6aa978b07095512ec
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-04-01 21:20:05 +0530

    asgdyas

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -24,7 +24,7 @@ export AWS_PROFILE=prod; python git-test/aws-cert.py --action=LISTEXPIRING --day
 
 cat newfile.txt
 
-awk '{print $0"<br></br>"}' confluence > msg
+awk '{print $0"<br></br>"}' newfile.txt > msg
 value="$(cat msg)"
 version=$(curl -u $Username:$Password $url | python -mjson.tool| grep number| awk {'print $2'}|tr "," " ");echo $version;version=$((version+1));echo $version
 jq -n --arg id "$id" --arg key "$key" --arg value "$value" --arg version "$version" --arg  url "$url" '{"id": $id, "type":"page","url":$url,"title":"AWS Certificates","space":{"key": $key },"body":{"storage":{"value": $value,"representation":"storage"}},"version":{"number": $version }}' > con.json
