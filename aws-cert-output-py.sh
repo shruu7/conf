e958ab8405812251cf2f16d227df338f931d1422
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-04-01 20:58:56 +0530

    ads

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 #!/usr/bin/env python2
-aws configure set region us-east-1 --profile sandbox
-aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile sandbox
-aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile sandbox
+#aws configure set region us-east-1 --profile sandbox
+#aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID --profile sandbox
+#aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY --profile sandbox
 echo "first step" > newfile.txt
-echo "List of Certificates for Sanbox that are going to expire is as follows." >> newfile.txt
+echo "List of Certificates for Sandbox that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
-export AWS_PROFILE=$Envi; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
+python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
 
 aws configure set region us-east-1 --profile Nonprod
 aws configure set aws_access_key_id $access  --profile Nonprod
@@ -16,12 +16,15 @@ echo "Second step" >> newfile.txt
 echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
-export AWS_PROFILE=$envi; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
+export AWS_PROFILE=Nonprod; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
 
-#aws configure set region us-east-1 --profile prod
-#echo "Third step" >> newfile.txt
-#echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
-#echo "=================================================================" >> newfile.txt
-#echo "" >> newfile.txt
-#python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
+aws configure set region us-east-1 --profile prod
+aws configure set aws_access_key_id $accessprod  --profile prod
+aws configure set aws_secret_access_key $secretprod --profile sandbox
+echo "Third step" >> newfile.txt
+echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
+echo "=================================================================" >> newfile.txt
+echo "" >> newfile.txt
+python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
+export AWS_PROFILE=prod; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
 cat newfile.txt
