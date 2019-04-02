a413d40f764c31b6ac8e3151c48bfbc75a406af5
shruu7 <srushtipatare@Srushtis-MacBook-Pro.local>
2019-04-02 20:11:00 +0530

    wkdf

--- aws-cert-output-py.sh
+++ aws-cert-output-py.sh
@@ -4,7 +4,7 @@
 aws configure set region us-east-1 --profile prod
 aws configure set aws_access_key_id $accessprod  --profile prod
 aws configure set aws_secret_access_key $secretprod --profile prod
-echo "List of Certificates for Prod that are going to expire is as follows." >> newfile.txt
+echo "List of Certificates for PROD that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
 export AWS_PROFILE=prod; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
@@ -17,7 +17,7 @@ fi
 aws configure set region us-east-1 --profile Nonprod
 aws configure set aws_access_key_id $access  --profile Nonprod
 aws configure set aws_secret_access_key $secret --profile Nonprod
-echo "List of Certificates for Non-Prod that are going to expire is as follows." >> newfile.txt
+echo "List of Certificates for NON-PROD that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
 export AWS_PROFILE=Nonprod; python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt >> newfile.txt
@@ -28,7 +28,7 @@ then
 fi
 
 unset AWS_PROFILE
-echo "List of Certificates for Sandbox that are going to expire is as follows." >> newfile.txt
+echo "List of Certificates for SANDBOX that are going to expire is as follows." >> newfile.txt
 echo "=================================================================" >> newfile.txt
 echo "" >> newfile.txt
 python git-test/aws-cert.py --action=LISTEXPIRING --daysleft=90 >> newfile.txt
